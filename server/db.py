232f21d9ed37bd6f75a5aff9942f5363afbea7c6
rpkenney <kenneyrobert109@gmail.com>
2023-12-19 23:14:18 -0500

    db

--- server/db.py
+++ server/db.py
@@ -3,10 +3,22 @@ import psycopg2
 def get_conn():
     return psycopg2.connect(host="localhost",dbname="crux_db", user="postgres", password="test1234")
 
+
+#prob could make this one function with optional params but not sure what is easier
 def execute(query, params):
     conn = get_conn()
     cursor = conn.cursor()
     cursor.execute(query, params)
+    result = cursor.fetchall()
+    conn.commit()
+    conn.close()
+
+    return result
+
+def execute(query):
+    conn = get_conn()
+    cursor = conn.cursor()
+    cursor.execute(query)
     result = cursor.fetchall()
     conn.commit()
     conn.close()
