966d5cba5e71cf3480c3f024d4aa0f1287b468fd
krao2099 <getkiranrao@gmail.com>
2023-12-26 02:04:57 -0500

    fuckin sidebar

--- frontend/home/home.js
+++ frontend/home/home.js
@@ -1,9 +1,94 @@
-var mymap = L.map('map').setView([51.505, -0.09], 13);
-
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(mymap);
+// config map
+let config = {
+    minZoom: 7,
+    maxZoom: 18,
+  };
+  // magnification with which the map will start
+  const zoom = 18;
+  // co-ordinates
+  const lat = 52.22977;
+  const lng = 21.01178;
+  
+  // calling map
+  const map = L.map("map", config).setView([lat, lng], zoom);
+  
+  // Used to load and display tile layers on the map
+  // Most tile servers require attribution, which you can set under `Layer`
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+  
+  // --------------------------------------------------
+  // sidebar
+  
+  const menuItems = document.querySelectorAll(".menu-item");
+  const sidebar = document.querySelector(".sidebar");
+  const buttonClose = document.querySelector(".close-button");
+  
+  menuItems.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      const target = e.target;
+  
+      if (
+        target.classList.contains("active-item") ||
+        !document.querySelector(".active-sidebar")
+      ) {
+        document.body.classList.toggle("active-sidebar");
+      }
+  
+      // show content
+      showContent(target.dataset.item);
+      // add active class to menu item
+      addRemoveActiveItem(target, "active-item");
+    });
+  });
+  
+  // close sidebar when click on close button
+  buttonClose.addEventListener("click", () => {
+    closeSidebar();
+  });
+  
+  // remove active class from menu item and content
+  function addRemoveActiveItem(target, className) {
+    const element = document.querySelector(`.${className}`);
+    target.classList.add(className);
+    if (!element) return;
+    element.classList.remove(className);
+  }
+  
+  // show specific content
+  function showContent(dataContent) {
+    const idItem = document.querySelector(`#${dataContent}`);
+    addRemoveActiveItem(idItem, "active-content");
+  }
+  
+  // --------------------------------------------------
+  // close when click esc
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      closeSidebar();
+    }
+  });
+  
+  // close sidebar when click outside
+  document.addEventListener("click", (e) => {
+    if (!e.target.closest(".sidebar")) {
+      closeSidebar();
+    }
+  });
+  
+  // --------------------------------------------------
+  // close sidebar
+  
+  function closeSidebar() {
+    document.body.classList.remove("active-sidebar");
+    const element = document.querySelector(".active-item");
+    const activeContent = document.querySelector(".active-content");
+    if (!element) return;
+    element.classList.remove("active-item");
+    activeContent.classList.remove("active-content");
+  }
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -18,9 +103,9 @@ function showPosition(position) {
     var longitude = position.coords.longitude;
 
     //alert("Latitude: " + latitude + "\nLongitude: " + longitude);
-    mymap.setView([latitude, longitude], 13);
+    map.setView([latitude, longitude], 13);
 
-    var marker = L.marker([latitude, longitude]).addTo(mymap);
+    var marker = L.marker([latitude, longitude]).addTo(map);
 
     marker.bindPopup("<b>Hello!</b><br>You are here!").openPopup();
 }
